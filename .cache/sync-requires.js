24b3772c6c5d87dfb5fb03787302844a6882685d
Art Bachmann <bachmann.art@gmail.com>
2020-03-10 09:07:24 +0200

    initial commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,10 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/art/Desktop/JSandFriends/Intermediate-Gatsby/gatsby-intermediate/sites/theme-dev/.cache/dev-404-page.js")))
+}
+
